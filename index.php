8d87aa89edc711df88797f9037a12aa0c8c5b1f9
maneritu <jdrr0505@gmail.com>
2016-11-04 19:31:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 
+<?php 
+require('conectar_base_de_datos.php');
+	$nombre = $_POST['nombre'];
+	$titulo = $_POST['titulo'];
+	$fecha = $_POST['fecha'];
+	$descripcion = $_POST['descripcion'];
+
+	$consulta = "INSERT INTO eventos (nombre, titulo, descripcion, fecha) VALUES ('$nombre', '$titulo', '$descripcion', '$fecha')";
+	$resultadoConsulta = $mysqli->query($consulta);
+
+	try {
+		if ($resultadoConsulta > 0) {
+
+			throw new Exception(1);
+			
+		}else{
+
+			throw new Exception(2);
+		}
+		
+	} catch (Exception $guardar) {
+		echo $guardar->getMessage();
+		
+	}
+?>
